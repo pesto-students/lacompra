d5d2371868aee20399e5b5d6a9910202ccaca6e2
Amritanshu rai <amritanshurai13@gmail.com>
2021-05-20 15:18:17 +0530

    chore: base css variables added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
@@ -11,11 +10,8 @@ const App = () => {
     })
   }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          Lacompra
-      </header>
+    <div>
+      Frontend Lacompra
     </div>
   );
 }
