33941a38c465bf907e85ec9e6391cbc9557fa92e
Amritanshu rai <amritanshurai13@gmail.com>
2021-05-28 01:21:47 +0530

    feat: footer developed

--- clientv2/src/App.js
+++ clientv2/src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/header/Header.jsx";
 import Hero from "./components/hero/Hero.jsx";
 import ProductCarousel from "./components/productCarousel/ProductCarousel.jsx";
 import Testimonial from "./components/testimonial/Testimonial.jsx";
+import Footer from "./components/footer/Footer.jsx";
+
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <ProductCarousel />
         <Testimonial />
       </main>
+      <Footer />
     </>
   );
 }
---
+++ clientv2/src/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import "./footer.styles.scss";
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ul>
+        <li>
+          <span>© La Compra 2021.</span>
+        </li>
+        <div>
+          <span>Developed by: </span>
+          <li>
+            <a href="https://www.rai.codes">Amritanshu</a>
+          </li>
+          <span>&</span>
+          <li>
+            <a href="https://github.com/TjGanesh">Ganesh</a>
+          </li>
+        </div>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
