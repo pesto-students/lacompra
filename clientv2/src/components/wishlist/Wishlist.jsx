916c3cfc685b2102cd060621e0bdcbacddfe31b3
Amritanshu rai <amritanshurai13@gmail.com>
2021-06-03 15:47:39 +0530

    feat: move to cart functionality implemented in wishlist

--- clientv2/src/components/wishlist/Wishlist.jsx
+++ clientv2/src/components/wishlist/Wishlist.jsx
@@ -1,15 +1,41 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getWishlist, deleteFromWishlist } from "./wishlistSlice";
+import { addToCart } from "../cart/cartSlice";
+
 import "./wishlist.styles.scss";
 const Wishlist = () => {
   const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+
   const { loading, wishlistItems } = useSelector((state) => state.wishlist);
 
   useEffect(() => {
     dispatch(getWishlist());
     // eslint-disable-next-line
   }, []);
+  const handleMoveToCart = (item) => {
+    const cartItemsTransformed = [];
+    cartItems.forEach((cartItem) => {
+      if (item.id !== cartItem.product.id) {
+        cartItemsTransformed.push({
+          product: cartItem.product.id,
+          count: cartItem.product.count,
+          size: cartItem.product.size,
+        });
+      }
+    });
+    const sizeAvailable = ["s", "m", "l", "xl"].find((size) => {
+      return item[size] > 0;
+    });
+    cartItemsTransformed.push({
+      product: item.id,
+      count: 1,
+      size: sizeAvailable,
+    });
+    dispatch(addToCart(cartItemsTransformed));
+    dispatch(deleteFromWishlist(item.id));
+  };
   if (loading === "loading") return <div>...loading</div>;
   return (
     <section className="wishlist">
@@ -19,9 +45,10 @@ const Wishlist = () => {
         <div className="product" key={item.id}>
           <img src={item.images[0]} alt={item.title} />
           <h4>{item.title}</h4>
-
           <div className="btn">
-            <button className="btn_cart">move to cart</button>
+            <button onClick={() => handleMoveToCart(item)} className="btn_cart">
+              move to cart
+            </button>
             <button onClick={() => dispatch(deleteFromWishlist(item.id))}>
               remove
             </button>
@@ -33,3 +60,6 @@ const Wishlist = () => {
 };
 
 export default Wishlist;
+
+//TODO
+//1) FIX: if user cart is not fetched; it is treated as empty and simply gets replaced by whatever is added by wishlist in it.
